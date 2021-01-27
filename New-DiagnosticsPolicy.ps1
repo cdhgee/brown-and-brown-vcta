f13e4c6a3ad2daf650d3adc509102d0e91520e08
David Gee <cdhgee@users.noreply.github.com>
2021-01-27 15:47:27 -0600

    Add policy deployment script

---
+++ New-DiagnosticsPolicy.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory = $true)]
+  [ValidateNotNullOrEmpty()]
+  [string]$ManagementGroupId,
+  [Parameter(Mandatory = $true)]
+  [ValidateNotNullOrEmpty()]
+  [string]$StorageAccount,
+  [Parameter(Mandatory = $true)]
+  [ValidateNotNullOrEmpty()]
+  [string]$TagName,
+  [Parameter(Mandatory = $true)]
+  [ValidateNotNullOrEmpty()]
+  [string]$TagValue
+)
+
+$params = @{
+  storageAccount    = $StorageAccount
+  tagName           = $TagName
+  tagValue          = $TagValue
+  managementGroupId = $ManagementGroupId
+}
+
+New-AzManagementGroupDeployment -ManagementGroupId $ManagementGroupId `
+  -TemplateFile "$PSScriptRoot/arm-templates/deploy-vcta-diagnostics.json" `
+  -TemplateParameterObject $params
